599ddc6a548bfb536aa04bc17917ffb1e97217ce
KalachevGleb <gleb.kalachev@yandex.ru>
2017-05-24 22:29:07 +0300

    Добавил комментарии к функциям в примере

--- test/terms/src/Main.hs
+++ test/terms/src/Main.hs
@@ -36,9 +36,9 @@ import Data.List
 import Data.String
 import Control.Monad
 
-type TermSym = String
 
-foreach :: [a']->(a'->Bool)->Bool
+-- | 'foreach' function, for list @l@ and predicate @pred@ checks whether @pred@ is true for all elements from @l@
+foreach :: [a]->(a->Bool)->Bool
 
 takewhile l pred = case l of
     []   -> []
@@ -64,37 +64,47 @@ sumf f l = case l of
 --data TermElement f v = Const f | Var v
 --    deriving (Show, Eq, Ord)
 
-
+-- | term with arbitrary type @f@ of functional symbols and type @v@ of variable symbols
 data Term f v
-    = Const f
-    | Var v
-    | Fun f [Term f v]
-    | Empty
+    = Const f          -- ^ constant node (may be used instead of function node with 0 arguments)
+    | Var v            -- ^ variable node
+    | Fun f [Term f v] -- ^ function node with argument list. Each argument is also a term.
     deriving (Eq, Ord)
 
+-- | Default type of functional and variable symbols
+type TermSym = String
+
+-- | 'STerm' is short name for term with default type of functional and variable symbols
 type STerm = Term TermSym TermSym
 
+-- | 'TermReference' specifies subterm with position in the whole term.
+--    stores current subterm and sequence of parents from current subterm parent to term root
 data TermReference f v = TRef [Term f v]
     deriving (Show, Eq, Ord)
 
+-- | 'STermReference' is short name for 'TermReference' with default type of functional and variable symbols
 type STermReference = TermReference TermSym TermSym
 
-termRefList ref = case ref of TRef x -> x
+--termRefList ref = case ref of TRef x -> x
 
+-- | represents term element : functional symbol of type @f@ or variable of type @v@
 data TermElement f v = TermVar v | TermSym f
     deriving (Show, Eq, Ord)
 
+-- | short name for term element with default types of functional and variable symbols
 type STermElement = TermElement TermSym TermSym
 
+-- | 'ITerm' represents interface of object which is similar to STerm.
 class ITerm t where
-    header :: t -> STermElement
-    subterms :: t -> [STerm]
-    subtermRefs :: t -> [STermReference]
-    operands :: t -> [STerm]
-    operandRefs :: t -> [STermReference]
-    term :: t-> STerm
-    termref :: t -> STermReference
+    header :: t -> STermElement          -- ^ returns term header
+    subterms :: t -> [STerm]             -- ^ enumerates subterms of current term
+    subtermRefs :: t -> [STermReference] -- ^ enumerate references to subterms of current term
+    operands :: t -> [STerm]             -- ^ enumerate operands of current term
+    operandRefs :: t -> [STermReference] -- ^ enumerate references to operands of current term
+    term :: t-> STerm                    -- ^ converts given object to 'STerm' type
+    termref :: t -> STermReference       -- ^ returns reference to current term or subterm
 
+-- | 'STerm' represents term itself
 instance ITerm STerm where
     subterms t = case t of
         Fun f l -> t : concatMap subterms l
@@ -113,6 +123,7 @@ instance ITerm STerm where
     term t = t
     termref t = TRef [t]
 
+-- | 'STermReference' --- ссылка на подтерм; интерфейс 'ITerm' осуществляет работу с этим подтермом.
 instance ITerm STermReference where
     subterms (TRef (t:ts)) = subterms t
     subterms (TRef []) = []
@@ -126,12 +137,13 @@ instance ITerm STermReference where
         subrefs ref = ref : concatMap subrefs (operandRefs ref)
 
     term (TRef (t:ts)) = t
-    term (TRef []) = Empty
     termref t = t
 
-parentRefs (TRef (t:s:ts)) = ts:parentRefs (TRef (s:ts))
-parentRefs _ = []
-parents (TRef (t:ts)) = ts
+-- | returns list of parent subterms of current subterm
+parentRefs (TRef ref) = tail ref --ts:parentRefs (TRef (s:ts))
+--parentRefs _ = []
+--parents (TRef (t:ts)) = ts
+
 
 class (Show s) => LogSymbol s where
    str :: s -> String
@@ -155,14 +167,14 @@ instance (LogSymbol f, LogSymbol v) => Show (Term f v) where
             sumstr [a] = a
             sumstr (x:y:l) = x++","++sumstr (y:l)
 
-class IConvertible a b where
-    convert :: a -> b
-
-instance IConvertible f (Term f v) where
-    convert = Const
-
-instance IConvertible STermReference STerm where
-    convert = term
+--class IConvertible a b where
+--    convert :: a -> b
+--
+--instance IConvertible f (Term f v) where
+--    convert = Const
+--
+--instance IConvertible STermReference STerm where
+--    convert = term
 
 -- show
 myFunction :: forall a. Ord a => [a] -> [(a, a)]
@@ -175,48 +187,16 @@ myFunction inputList = zip sortedList nubbedList
 -- /show--IConvertible x (Term f v)
 infixr 5 &
 (&) :: f -> [Term f v] -> Term f v
-(&) = Fun
+f&[] = Const f
+f&l = Fun f l
 
 lengthf term = case term of
     Fun f l -> 1 + sumf lengthf l
     _ -> 1
 
-
-refVal (TRef []) = Empty
+-- | converts 'TermReference' to 'Term'. For 'Sterm' and 'STermReference' same as 'term'
 refVal (TRef (x:xs)) = x
-
-
---refVal :: TermReference f v -> Term f v
---refVal (term,index) = case term of
---    Var v -> Var v
---    Const c -> Const c
---    Empty -> Empty
---    Fun f l -> if index == 0 then term else fnd l index
---        where
---            fnd ll index = case ll of
---                [] -> Empty
---                x:xs -> if lx <= index then fnd xs (index-lx) else refVal(x, index)
---                    where lx = lengthf x
-
--- returns list of operands of given term
---operands :: Term f v -> [Term f v]
---operands term = case term of
---    Fun f l -> l
---    _ -> []
-
-
--- returns head of term (symbol or variable)
---header :: Term f v -> TermElement f v
---header term = case term of
---    Var v -> TermVar v
---    Const c -> TermSym c
---    Fun f l -> TermSym f
-
--- returns list of subterms of current term
---subterms :: Term f v -> [Term f v]
---subterms t = case t of
---    Fun f l -> t : concatMap subterms l
---    _ -> [t]
+--refVal (TRef []) = Empty
 
 -- replaces subterms from list s by corresponding subterms of list t
 replaceTerms x s t = case elemIndex x s of
@@ -225,9 +205,11 @@ replaceTerms x s t = case elemIndex x s of
         Fun f l -> Fun f (map (\y -> replaceTerms y s t) l)
         _ -> x
 
+-- | test function for term replacements
 repl t 0 = t
 repl t n = replaceTerms (repl t (n-1)) ["g"&[Var "x"], Var "y"]  ["f"&[Var "y", "g"&[Var "x"]], "g"&[Var "x"]]
 
+-- | measures time of computation.
 time :: IO t -> IO t
 time a = do
     start <- getCPUTime
@@ -241,7 +223,10 @@ time a = do
 rmdups :: (Ord a) => [a] -> [a]
 rmdups = map head . group . sort
 
-
+-- | hasMatch unify given term with pattern.
+--   If term @trm@ can be obtained from term @patt@ by some substitution x1->t1,...,xn->tn,
+--     then returns 'Just' [(x1,t1),...,(xn,tn)].
+--   Otherwise returns 'Nothing'
 hasMatch :: (Ord f, Ord v, Ord v1) => Term f v -> Term f v1 -> Maybe [(v1, Term f v)]
 
 hasMatch trm patt = case r of
@@ -259,6 +244,7 @@ hasMatch trm patt = case r of
         isMapping ((x1,y1):(x2,y2):xs) = (x1/=x2 || y1==y2) && isMapping ((x2,y2):xs)
         isMapping _ = True
 
+-- | 'findMatches' @t@ @pattern@ enumerates all subterms of term @t@ that matches given pattern
 findMatches :: STerm -> STerm -> [(STerm, [(TermSym, STerm)])]
 findMatches trm patt = concatMap m (subterms trm)
     where
@@ -266,20 +252,23 @@ findMatches trm patt = concatMap m (subterms trm)
             Nothing -> []
             Just l -> [(x,l)]
 
-class ScanState st where
+-- | Класс 'IScanState' представляет интерфейс состояния сканирования задачи
+class IScanState st where
     level :: st -> Int
     new :: st -> Bool
 
-class Problem p where
-    premises :: p -> [STerm]
-    conditions :: p -> [STerm]
-    goals :: p -> [STerm]
+-- | Класс 'IProblem' представляет интерфейс задачи
+class IProblem p where
+    assumptions  :: p -> [STerm] -- ^ посылки задачи (в решателе)
+    conditions   :: p -> [STerm] -- ^ условия задачи (в решателе)
+    instructions :: p -> [STerm] -- ^ цели задачи (в решателе)
 
-solvePls :: (ScanState st, Problem p) => (st, p) -> STermReference-> Int -> Int -> Bool -> (st, p)
-zamenavhozhdeniya :: (ScanState st, Problem p) => st->p->STermReference-> Int -> Int ->STerm->[[STerm]]->(st,p)
+solvePls :: (IScanState st, IProblem p) => (st, p) -> STermReference-> Int -> Int -> Bool -> (st, p)
+zamenavhozhdeniya :: (IScanState st, IProblem p) => st->p->STermReference-> Int -> Int ->STerm->[[STerm]]->(st,p)
 
 zamenavhozhdeniya s p x2 x3 x4 x5 u = (s,p)
 
+-- | checks whether current term reference points to functional symbol (not a constant or variable)
 internal :: TermReference f v -> Bool
 internal (TRef [a]) = True
 internal _ = False
@@ -300,16 +289,36 @@ solvePls (state, x1) x2 x3 x4 x5 =
                     let x8 = [term x9 | x9 <- operandRefs x2, x9/=x6, x9/=x7]
                     let u = [["priem"&[], "pls"&[], "list"&["1"&[],"2"&[]], "secondsubterm"&[]]]
                     return (zamenavhozhdeniya state x1 x2 x3 x4 ("pls" & (x8++["0"&[]])) u)
-            ++
-            do
-                x6 <- operandRefs x2
-                do
-                    x7 <- operandRefs x2
-                    guard (x6 /= x7)
-                    guard (term x6 == term x7)
-                    let x8 = [term x9 | x9 <- operandRefs x2, x9/=x6, x9/=x7]
-                    let u = [["priem"&[], "pls"&[], "list"&["1"&[],"2"&[]], "secondsubterm"&[]]]
-                    return (zamenavhozhdeniya state x1 x2 x3 x4 ("pls" & (x8++["0"&[]])) u)
+--                ++
+--                do
+--                    guard (header x6 == "pls")
+--                    guard (x6 /= x7)
+--                    guard (term x6 == term x7)
+--                    let x8 = [term x9 | x9 <- operandRefs x2, x9/=x6, x9/=x7]
+--                    let u = [["priem"&[], "pls"&[], "list"&["1"&[],"2"&[]], "secondsubterm"&[]]]
+--                    return (zamenavhozhdeniya state x1 x2 x3 x4 ("pls" & (x8++["0"&[]])) u)
+
+--	internal(x2);
+--	branch {
+--		level(0d);
+--		novij;
+--		branch {
+--			operand(x2, x6);
+--			branch {
+--				operand(x2, x7);
+--				x7 != x6;
+--				ravnietermi(x6, x7);
+--				x8 = listof(subterm(x9) where x9 : operand(x2, x9), x9 != x6, x9 != x7);
+--				kontrolpriema(pls, 12d, secondsubterm);
+--				zamenavkhozhdeniya(x2, x3, x4, x1, unisborka(pls, append(x8, 0d)), [[priem, pls, 12d, secondsubterm]]);
+--				rescan;
+--			}
+--			branch {
+--				kontrolpriema(pls, 5d, spuskoperandov);
+--				simvol(x6, pls);
+--				zamenavkhozhdeniya(x2, x3, x4, x1, spuskoperandov(x2, x6), [[priem, pls, 5d, spuskoperandov]]);
+--				rescan;
+--			}
 
 
 test n =
